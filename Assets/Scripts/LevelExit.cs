cf4904bff5440fd7a74788b9778a6d6c30a335d2
cengiss <egemen690@gmail.com>
2023-07-18 18:29:28 +0300

    First Commit

---
+++ Assets/Scripts/LevelExit.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 2f;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player"){
+        StartCoroutine(LoadNextLevel1());
+        }
+    }
+
+    IEnumerator LoadNextLevel1()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings){
+            nextSceneIndex = 0;
+        }
+
+        FindObjectOfType<ScenePersist>().ResetScenePersist();
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+
+}
